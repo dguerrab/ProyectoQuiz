fcd0490dab04acfdd05abfc9df1ff26a593d91bc
aperezmi <angel.perez.miguel@alumnos.upm.es>
2015-04-28 11:46:30 +0200

    Layout y express-partials

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 // Importa enrutadores
+var partials = require('express-partials');
 var routes = require('./routes/index');
-
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(partials());
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Instala enrutadores
 app.use('/', routes);
+
 
 
 // catch 404 and forward to error handler
